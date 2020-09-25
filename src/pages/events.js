b4b03bb2b137eb62ac0ccc7ce89ca62b79425052
Richard Xia <richardxia@richardxia.com>
2020-09-25 00:05:44 -0700

    Move index.js files out of unnecessary directories.

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,6 @@
 import React from "react";
-import EventsView from "../../components/pages/events/EventsView";
-import Layout from "../../layouts/Layout";
+import EventsView from "../components/pages/events/EventsView";
+import Layout from "../layouts/Layout";
 
 // Sync route definition
 export default () => (
--- src/pages/get-involved.js
+++ src/pages/get-involved.js
@@ -1,6 +1,6 @@
 import React from "react";
-import GetInvolvedView from "../../components/pages/get-involved/GetInvolvedView";
-import Layout from "../../layouts/Layout";
+import GetInvolvedView from "../components/pages/get-involved/GetInvolvedView";
+import Layout from "../layouts/Layout";
 
 // Sync route definition
 export default () => (

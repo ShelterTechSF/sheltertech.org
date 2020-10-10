cfce31ece7b1966ff38c375307d4e710a8c595b9
Ivan Lee <ilee.code@gmail.com>
2020-10-10 01:22:52 -0700

    Add parameters to Template const

--- src/components/inline/DonationProjectCard/DonationProjectCard.stories.js
+++ src/components/inline/DonationProjectCard/DonationProjectCard.stories.js
@@ -1,14 +1,20 @@
 import React from "react";
 import DonationProjectCard from "./DonationProjectCard";
-import background from "./blackbackground.jpg";
+import background from "./coralreef.jpg";
 
 export default {
   title: "Inline/DonationProjectCard",
   component: DonationProjectCard,
 };
 
-const Template = () => (
-  <div style={{ backgroundImage: `url(${background})` }}>
+const Template = ({ title, subtitle }) => (
+  <div
+    style={{
+      backgroundImage: `url(${background})`,
+      backgroundSize: "240px 240px",
+      backgroundRepeat: "no-repeat",
+    }}
+  >
     <DonationProjectCard title="Star Hotel SRO" subtitle="280 People" />
   </div>
 );

6f2fb145ddaf5e0d8f5d666b31213238b18093d3
Candy Wang <candywang013@gmail.com>
2020-10-11 19:07:36 -0700

    Add theme property and rename alt to imageAlt

--- src/components/block/ProgramCard/ProgramCard.stories.js
+++ src/components/block/ProgramCard/ProgramCard.stories.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ProgramCard from "./ProgramCard";
-import img from "./sample.jpg";
+import img from "./stories/sample.jpg";
 
 export default {
   title: "Block/ProgramCard",
   component: ProgramCard,
 };
 
-const Template = ({ image, alt, title, description }) => (
+const Template = ({ theme, image, imageAlt, title, description }) => (
   <ProgramCard
+    theme={theme}
     image={image}
-    alt={alt}
+    imageAlt={imageAlt}
     title={title}
     description={description}
   />
@@ -18,8 +19,9 @@ const Template = ({ image, alt, title, description }) => (
 
 export const DefaultProgramCard = Template.bind({});
 DefaultProgramCard.args = {
+  theme: "light",
   image: img,
-  alt: "Image of ShelterTech booth with 4 volunteers smiling.",
+  imageAlt: "Image of ShelterTech booth with 4 volunteers smiling.",
   title: "ShelterConnect",
   description:
     "Every year, our ShelterConnect program provides 3XXX homeless and housing insecure people with free and unlimited internet access and we plan to provide every homeless individual with the digital infrastructure that meets their needs by 2024.",

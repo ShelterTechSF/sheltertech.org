1d1102b9fe49ecf40b4716da5c9fbe6a729e137b
David Agustin <dagustin@myvest.com>
2020-10-12 04:09:28 -0700

    Change subtitle property name to title. Change Value String of buttonText to Capitalize First Letter of Words

--- src/components/block/FooterCTACard/FooterCTACard.stories.js
+++ src/components/block/FooterCTACard/FooterCTACard.stories.js
@@ -6,14 +6,14 @@ export default {
   component: FooterCTACard,
 };
 
-const Template = ({ subtitle, body, buttonText }) => (
+const Template = ({ title, body, buttonText }) => (
   <div style={{ backgroundColor: "black", width: "480px" }}>
-    <FooterCTACard subTitle={subtitle} body={body} buttonText={buttonText} />
+    <FooterCTACard title={title} body={body} buttonText={buttonText} />
   </div>
 );
 export const DefaultFooterCTACard = Template.bind({});
 DefaultFooterCTACard.args = {
-  subtitle: "Get Involved",
+  title: "Get Involved",
   body: "Volunteer with us and put your professional skills to good use.",
-  buttonText: "JOIN US",
+  buttonText: "Join Us",
 };

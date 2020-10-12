d02bb4682bf7ddbbb317a477bfda7fb94ad9a3aa
Candy Wang <candywang013@gmail.com>
2020-10-11 18:23:56 -0700

    Destructure theme prop, create conditional rendering for theme styling, add theme propType, and change alt to imageAlt

--- src/components/block/ProgramCard/ProgramCard.js
+++ src/components/block/ProgramCard/ProgramCard.js
@@ -2,10 +2,13 @@ import PropTypes from "prop-types";
 import React from "react";
 import s from "./ProgramCard.module.css";
 
-const ProgramCard = ({ image, alt, title, description }) => (
-  <div className={s.programCardContainer}>
+const ProgramCard = ({ theme, image, imageAlt, title, description }) => (
+  <div
+    className={`${s.programCardContainer} 
+    ${theme === "light" ? s.light : s.dark}`}
+  >
     <div>
-      <img className={s.img} src={image} alt={alt} />
+      <img className={s.img} src={image} alt={imageAlt} />
     </div>
     <div className={s.title}>{title}</div>
     <div className={s.description}>{description}</div>
@@ -13,8 +16,9 @@ const ProgramCard = ({ image, alt, title, description }) => (
 );
 
 ProgramCard.propTypes = {
+  theme: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };

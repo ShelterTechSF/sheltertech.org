9ab8f4fc0e1d52511fbc05cba20e110ff65bfef2
David Agustin <dagustin@myvest.com>
2020-10-12 03:52:52 -0700

    Remove DefaultProps. Make All propTypes Required. Change subtitle Class and Property to title. Change Class button to buttonWrapper

--- src/components/block/FooterCTACard/FooterCTACard.js
+++ src/components/block/FooterCTACard/FooterCTACard.js
@@ -3,24 +3,18 @@ import React from "react";
 import Button from "../../inline/Button";
 import s from "./FooterCTACard.module.css";
 
-const FooterCTACard = ({ subtitle, body, buttonText }) => (
+const FooterCTACard = ({ title, body, buttonText }) => (
   <div className={s.container}>
-    <div className={s.subtitle}>{subtitle}</div>
+    <div className={s.title}>{title}</div>
     <div className={s.body}>{body}</div>
-    <div className={s.button}>
+    <div className={s.buttonWrapper}>
       <Button>{buttonText}</Button>
     </div>
   </div>
 );
 
 FooterCTACard.propTypes = {
-  subtitle: PropTypes.string,
-  body: PropTypes.string,
-  buttonText: PropTypes.string,
-};
-
-FooterCTACard.defaultProps = {
-  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
 };

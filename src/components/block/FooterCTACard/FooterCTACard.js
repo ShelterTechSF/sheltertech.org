882728a6993531893157bcbedfdec7093a6dffa2
David Agustin <dagustin@myvest.com>
2020-10-13 23:44:39 -0700

    Change body Variable to description

--- src/components/block/FooterCTACard/FooterCTACard.js
+++ src/components/block/FooterCTACard/FooterCTACard.js
@@ -3,10 +3,10 @@ import React from "react";
 import Button from "../../inline/Button";
 import s from "./FooterCTACard.module.css";
 
-const FooterCTACard = ({ title, body, buttonText }) => (
+const FooterCTACard = ({ title, description, buttonText }) => (
   <div className={s.container}>
     <div className={s.title}>{title}</div>
-    <div className={s.body}>{body}</div>
+    <div className={s.description}>{description}</div>
     <div className={s.buttonWrapper}>
       <Button>{buttonText}</Button>
     </div>
@@ -15,7 +15,7 @@ const FooterCTACard = ({ title, body, buttonText }) => (
 
 FooterCTACard.propTypes = {
   title: PropTypes.string.isRequired,
-  body: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
 };
 

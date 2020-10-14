471eaef0eff47ed33a69e37d3b848fa665a44979
David Agustin <dagustin@myvest.com>
2020-10-13 23:45:30 -0700

    Change body Variable to description

--- src/components/block/FooterCTACard/FooterCTACard.stories.js
+++ src/components/block/FooterCTACard/FooterCTACard.stories.js
@@ -6,14 +6,21 @@ export default {
   component: FooterCTACard,
 };
 
-const Template = ({ title, body, buttonText }) => (
+const Template = ({ title, description, buttonText }) => (
   <div style={{ backgroundColor: "black", width: "480px" }}>
-    <FooterCTACard title={title} body={body} buttonText={buttonText} />
+    <FooterCTACard
+      title={title}
+      description={description}
+      buttonText={buttonText}
+    />
   </div>
 );
+
 export const DefaultFooterCTACard = Template.bind({});
+
 DefaultFooterCTACard.args = {
   title: "Get Involved",
-  body: "Volunteer with us and put your professional skills to good use.",
+  description:
+    "Volunteer with us and put your professional skills to good use.",
   buttonText: "Join Us",
 };

6aeedf4747c87581fa55dadb4913b75b789d489a
Ivan Lee <ilee.code@gmail.com>
2020-10-17 13:44:56 -0700

    Implement HomePageParagraph to render text

---
+++ src/components/grid-aware/HomePageLargeParagraph/HomePageParagraph.js
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import s from "./HomePageParagraph.module.css";
+
+const HomePageParagraph = ({ text }) => <p className={s.text}>{text}</p>;
+
+HomePageParagraph.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+
+
+export default HomePageParagraph;
+

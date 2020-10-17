1627d80e50f5d9b1ca3f7165ca16be896c3d49e8
Ivan Lee <ilee.code@gmail.com>
2020-10-17 16:32:08 -0700

    Implement grid to component

--- src/components/grid-aware/HomePageLargeParagraph/HomePageParagraph.js
+++ src/components/grid-aware/HomePageLargeParagraph/HomePageParagraph.js
@@ -3,13 +3,46 @@ import React from "react";
 
 import s from "./HomePageParagraph.module.css";
 
-const HomePageParagraph = ({ text }) => <p className={s.text}>{text}</p>;
+const HomePageParagraphPropType = PropTypes.shape({
+  text: PropTypes.string.isRequired
+});
 
-HomePageParagraph.propTypes = {
+const HomePageParagraphBlock = ({ text }) => (
+  <div>
+    <p className={s.text}>{text}</p>
+  </div>
+)
+
+HomePageParagraphBlock.propTypes = {
   text: PropTypes.string.isRequired,
 };
 
+const HomePageParagraph = ({
+  text
+}) => {
 
+  const GridAreaMiddle = () => (
+    <div className={s.gridAreaMiddle}>
+        <HomePageParagraphBlock
+          text={text}
+        />
+    </div>
+  );
+
+  return (
+    <div className={s.bleedWrapper}>
+      <div className={s.bleedMainContent}>
+        <section className={s.gridParent}>
+          <GridAreaMiddle />
+        </section>
+      </div>
+    </div>
+  );
+};
+
+HomePageParagraph.propTypes = {
+  text: HomePageParagraphPropType.isRequired,
+};
 
 export default HomePageParagraph;
 

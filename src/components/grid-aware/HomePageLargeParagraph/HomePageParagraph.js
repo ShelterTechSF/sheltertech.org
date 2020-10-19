0406cadf14341536f27a6531bf42ec817c5190dc
Ivan Lee <ilee.code@gmail.com>
2020-10-18 21:54:25 -0700

    Remove unnecessary divs

--- src/components/grid-aware/HomePageLargeParagraph/HomePageParagraph.js
+++ src/components/grid-aware/HomePageLargeParagraph/HomePageParagraph.js
@@ -3,40 +3,25 @@ import React from "react";
 
 import s from "./HomePageParagraph.module.css";
 
-const HomePageParagraphPropType = PropTypes.shape({
-  text: PropTypes.string.isRequired,
-});
-
-const HomePageParagraphBlock = ({ text }) => (
-  <div>
-    <p className={s.text}>{text}</p>
-  </div>
-);
-
-HomePageParagraphBlock.propTypes = {
-  text: PropTypes.string.isRequired,
-};
 
 const HomePageParagraph = ({ text }) => {
   const GridAreaMiddle = () => (
     <div className={s.gridAreaMiddle}>
-      <HomePageParagraphBlock text={text} />
+      <div className={s.text}>{text}</div>
     </div>
   );
 
   return (
     <div className={s.bleedWrapper}>
       <div className={s.bleedMainContent}>
-        <section className={s.gridParent}>
-          <GridAreaMiddle />
-        </section>
+        <GridAreaMiddle />
       </div>
     </div>
   );
 };
 
 HomePageParagraph.propTypes = {
-  text: HomePageParagraphPropType.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default HomePageParagraph;

0297fe78cc2856220d4ee7c0af48a41ee5dacfa9
David Agustin <dagustin@myvest.com>
2020-10-18 19:26:49 -0700

    Separate Each Button Component To Have Their Own Story. Change scriptAction Variable to onClick

--- src/components/inline/Button/Button.stories.js
+++ src/components/inline/Button/Button.stories.js
@@ -2,46 +2,50 @@ import React from "react";
 
 import Button from "./Button";
 
+const demoStyling = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  marginTop: "50vh",
+};
+
 export default {
   title: "Inline/Button",
   component: Button,
 };
 
-const Template = ({
-  externalLinkButton,
-  internalLinkButton,
-  scriptActionButton,
-}) => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-evenly",
-      alignItems: "center",
-      marginTop: "50vh",
-    }}
-  >
-    <Button button={externalLinkButton} />
-    <Button button={internalLinkButton} />
-    <Button button={scriptActionButton} />
+const Template = ({ button }) => (
+  <div style={demoStyling}>
+    <Button button={button} />
   </div>
 );
 
-export const DefaultButton = Template.bind({});
-DefaultButton.args = {
-  externalLinkButton: {
+export const ExternalLink = Template.bind({});
+ExternalLink.args = {
+  button: {
     text: "External Link",
     externalLink: "https://www.google.com",
   },
-  internalLinkButton: {
+};
+
+export const InternalLink = Template.bind({});
+InternalLink.args = {
+  button: {
     text: "Internal Link",
     internalLink: "/",
   },
-  scriptActionButton: {
-    text: "Script Action",
-    scriptAction: () => {
+};
+
+export const OnClickButton = Template.bind({});
+OnClickButton.args = {
+  button: {
+    text: "On Click",
+    onClick: (event) => {
       // eslint-disable-next-line no-console
       console.log("Modal, Dialogue or Popup");
+      // eslint-disable-next-line no-console
+      console.log(`event is ${event}`);
     },
   },
 };

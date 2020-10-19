e456e65769f8faf92c6d75caada824ddb80ddd20
David Agustin <dagustin@myvest.com>
2020-10-18 19:24:21 -0700

    Rename scriptAction Variable to onClick. Remove Extra propType layer. Remove div element

--- src/components/inline/Button/Button.js
+++ src/components/inline/Button/Button.js
@@ -1,45 +1,46 @@
+import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
 import s from "./Button.module.css";
 
 const Button = ({ button }) => {
-  let buttonView;
-
   if (button.externalLink) {
-    buttonView = (
-      <a rel="noreferrer" target="_blank" href={button.externalLink}>
-        <button type="button" className={s.button}>
-          {button.text}
-        </button>
+    return (
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href={button.externalLink}
+        className={s.button}
+      >
+        {button.text}
       </a>
     );
-  } else if (button.internalLink) {
-    buttonView = (
-      <a href={button.internalLink}>
-        <button type="button" className={s.button}>
-          {button.text}
-        </button>
-      </a>
+  }
+  if (button.internalLink) {
+    return (
+      <Link to={button.internalLink} className={s.button}>
+        {button.text}
+      </Link>
     );
-  } else {
-    buttonView = (
+  }
+  if (button.onClick) {
+    return (
       <button
         type="button"
         className={s.button}
-        onClick={() => {
-          button.scriptAction();
+        onClick={(event) => {
+          button.onClick(event);
         }}
       >
         {button.text}
       </button>
     );
   }
-
-  return <div>{buttonView}</div>;
+  throw new Error("PropTypes are invalid");
 };
 
-const ButtonPropType = PropTypes.oneOfType([
+Button.propTypes = PropTypes.oneOfType([
   PropTypes.exact({
     text: PropTypes.string,
     externalLink: PropTypes.string,
@@ -50,12 +51,8 @@ const ButtonPropType = PropTypes.oneOfType([
   }),
   PropTypes.exact({
     text: PropTypes.string,
-    scriptAction: PropTypes.func,
+    onClick: PropTypes.func,
   }),
-]);
-
-Button.propTypes = {
-  button: ButtonPropType.isRequired,
-};
+]).isRequired;
 
 export default Button;

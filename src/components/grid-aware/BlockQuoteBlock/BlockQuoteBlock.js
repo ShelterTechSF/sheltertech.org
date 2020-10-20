7f40baa5f625e483682c959ad5c7afd6cfea5978
Ivan Lee <ilee.code@gmail.com>
2020-10-20 14:52:47 -0700

    Add lint fixes

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
@@ -1,23 +1,25 @@
 import PropTypes from "prop-types";
 import React from "react";
 import s from "./BlockQuoteBlock.module.css";
-const BlockQuoteBlock = ({ quote, author }) => {
-const GridAreaMiddle = () => (
 
-  <div className={s.gridAreaMiddle}>
-    <div className={s.quote}>{quote}</div>
-    <br/>
-  <div className={s.author}>{author}</div>
-</div>
-);
-return (
-  <div className={s.bleedWrapper}>
-    <div className={s.bleedMainContent}>
-      <GridAreaMiddle />
+const BlockQuoteBlock = ({ quote, author }) => {
+  const GridAreaMiddle = () => (
+    <div className={s.gridAreaMiddle}>
+      <div className={s.quote}>{quote}</div>
+      <br />
+      <div className={s.author}>{author}</div>
     </div>
-  </div>
+  );
+
+  return (
+    <div className={s.bleedWrapper}>
+      <div className={s.bleedMainContent}>
+        <GridAreaMiddle />
+      </div>
+    </div>
   );
 };
+
 BlockQuoteBlock.propTypes = {
   quote: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,

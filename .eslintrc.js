3e00f8a3265c8a0846e8beac5cdd685a0f173176
Richard Xia <richardxia@richardxia.com>
2020-10-20 09:15:41 -0700

    Fail tests if console.error() calls are made.

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,12 +41,17 @@ module.exports = {
     },
     {
       // Jest
-      files: ["__mocks__/**", "jest/**"],
+      files: ["__mocks__/**", "jest/**", "**/*.test.js"],
       rules: {
         "import/no-extraneous-dependencies": [
           "error",
           { devDependencies: true },
         ],
+        // Airbnb disallows certain syntax because they may be expensive to run
+        // in a browser when transpiling to older versions of JavaScript. This
+        // is not a concern for our test code that runs on Node.js, since this
+        // newer syntax is natively supported.
+        "no-restricted-syntax": "off",
       },
       env: {
         // Jest injects itself as a varibable in test files

677a741cea6b4e65a2bea591a920f675bf52f545
Candy Wang <candywang013@gmail.com>
2020-10-21 01:57:04 -0700

    Refactor PartnersAndSponsorsBlock component to merge code from LogosBlock subcomponent, delete LogosBlock subcomponent

--- src/components/grid-aware/PartnersAndSponsorsBlock/PartnersAndSponsorsBlock.js
+++ src/components/grid-aware/PartnersAndSponsorsBlock/PartnersAndSponsorsBlock.js
@@ -3,33 +3,11 @@ import React from "react";
 
 import s from "./PartnersAndSponsorsBlock.module.css";
 
-/* PropType shapes */
-
 const LogoPropType = PropTypes.shape({
   id: PropTypes.number.isRequired,
   url: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
 });
-
-/* Subcomponent */
-
-const LogosBlock = ({ logoImages }) => (
-  <div>
-    <div className={s.logosWrapper}>
-      {logoImages.map((logo) => (
-        <div className={s.logoContainer}>
-          <img className={s.logo} key={logo.id} src={logo.url} alt={logo.alt} />
-        </div>
-      ))}
-    </div>
-  </div>
-);
-
-LogosBlock.propTypes = {
-  logoImages: PropTypes.arrayOf(LogoPropType).isRequired,
-};
-
-/* Main component */
 
 const PartnersAndSponsorsBlock = ({ title, partnersAndSponsors }) => {
   const GridAreaLeft = () => (
@@ -40,7 +18,18 @@ const PartnersAndSponsorsBlock = ({ title, partnersAndSponsors }) => {
 
   const GridAreaRight = () => (
     <div className={s.gridAreaRight}>
-      <LogosBlock logoImages={partnersAndSponsors} />
+      <div className={s.logosWrapper}>
+        {partnersAndSponsors.map((logo) => (
+          <div className={s.logoContainer}>
+            <img
+              className={s.logo}
+              key={logo.id}
+              src={logo.url}
+              alt={logo.alt}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 

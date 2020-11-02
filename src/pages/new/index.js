3b03940a81ac3dd564c92ac5828c6a40502eec9d
Candy Wang <candywang013@gmail.com>
2020-11-01 19:53:45 -0800

    Change ProgramsBlock component theme to "dark"

--- src/pages/new/index.js
+++ src/pages/new/index.js
@@ -61,7 +61,7 @@ export default () => (
       title="Our programs"
       programs={[
         {
-          theme: "light",
+          theme: "dark",
           image: shelterConnectImg,
           imageAlt: "Image of ShelterTech booth with 4 volunteers smiling.",
           title: "ShelterConnect",
@@ -69,7 +69,7 @@ export default () => (
             "Every year, our ShelterConnect program provides 3XXX homeless and housing insecure people with free and unlimited internet access and we plan to provide every homeless individual with the digital infrastructure that meets their needs by 2024.",
         },
         {
-          theme: "light",
+          theme: "dark",
           image: sfServiceGuideImg,
           imageAlt:
             "Image of the back of two people sitting where the woman on the left is pointing to the monitor of a Macbook as the man observes.",
@@ -78,7 +78,7 @@ export default () => (
             "Anyone with access to a smartphone, tablet, or computer can utilize this online directory of human services provided in San Francisco.",
         },
         {
-          theme: "light",
+          theme: "dark",
           image: communityDevelopmentImg,
           imageAlt: "Image of ShelterTech community member smiling.",
           title: "Community Development",

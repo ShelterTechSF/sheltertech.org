1bc4ca94f8736bd22a9b8e6e9aaf9a4bfa7f3ea3
Ivan Lee <ilee.code@gmail.com>
2020-11-01 23:55:47 -0800

    src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
@@ -1,28 +1,75 @@
+import Carousel from "nuka-carousel";
 import PropTypes from "prop-types";
 import React from "react";
 import s from "./BlockQuoteBlock.module.css";
 
-const BlockQuoteBlock = ({ quote, author }) => {
-  const GridAreaMiddle = () => (
-    <div className={s.gridAreaMiddle}>
-      <div className={s.quote}>{quote}</div>
-      <br />
-      <div className={s.author}>{author}</div>
-    </div>
-  );
+const BlockQuoteBlock = ({ quote, attribution }) => {
+  const quotes = [
+    { quote: "ShelterTech, Welcomes You!", attribution: "Richard Xia" },
+    { quote: "Please Donate!", attribution: "David Agustin" },
+    { quote: "Happy Holidays", attribution: "Candy Wang" },
+  ];
 
   return (
-    <div className={s.bleedWrapper}>
-      <div className={s.bleedMainContent}>
-        <GridAreaMiddle />
+    <Carousel
+      renderCenterLeftControls={function () {}}
+      renderCenterRightControls={function () {}}
+    >
+      <div className={s.bleedWrapper}>
+        <div className={s.bleedMainContent}>
+          <div className={s.gridAreaMiddle}>
+            <figure>
+              <blockquote className={s.quote}>"{quote}"</blockquote>
+              <figcaption className={s.attribution}>
+                &mdash;{attribution}
+              </figcaption>
+            </figure>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className={s.bleedWrapper}>
+        <div className={s.bleedMainContent}>
+          <div className={s.gridAreaMiddle}>
+            <figure>
+              <blockquote className={s.quote}>{quotes[0].quote}</blockquote>
+              <figcaption className={s.attribution}>
+                &mdash;{quotes[0].attribution}
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className={s.bleedWrapper}>
+        <div className={s.bleedMainContent}>
+          <div className={s.gridAreaMiddle}>
+            <figure>
+              <blockquote className={s.quote}>{quotes[1].quote}</blockquote>
+              <figcaption className={s.attribution}>
+                &mdash;{quotes[1].attribution}
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className={s.bleedWrapper}>
+        <div className={s.bleedMainContent}>
+          <div className={s.gridAreaMiddle}>
+            <figure>
+              <blockquote className={s.quote}>{quotes[2].quote}</blockquote>
+              <figcaption className={s.attribution}>
+                &mdash;{quotes[2].attribution}
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </Carousel>
   );
 };
 
 BlockQuoteBlock.propTypes = {
   quote: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
+  attribution: PropTypes.string.isRequired,
 };
 
 export default BlockQuoteBlock;
--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
@@ -1,4 +1,3 @@
-import Carousel from "nuka-carousel";
 import React from "react";
 import BlockQuoteBlock from "./BlockQuoteBlock";
 
@@ -7,19 +6,15 @@ export default {
   component: BlockQuoteBlock,
 };
 
-const Template = ({ quote, author }) => (
-  <Carousel renderCenterLeftControls={false} renderCenterRightControls={false}>
-    <BlockQuoteBlock quote={quote} author={author} />
-    <BlockQuoteBlock quote={quote} author={author} />
-    <BlockQuoteBlock quote={quote} author={author} />
-    <BlockQuoteBlock quote={quote} author={author} />
-    <BlockQuoteBlock quote={quote} author={author} />
-  </Carousel>
+const Template = ({ quote, attribution }) => (
+  <BlockQuoteBlock quote={quote} attribution={attribution} />
 );
 
 export const DefaultBlockQuoteBlock = Template.bind({});
 
 DefaultBlockQuoteBlock.args = {
-  quote: `"We've heard from San Franciscans, including as part of our recent strategic planning community outreach that it's hard to know where to go when you're looking for supportive services. It's hard to know where to start, and hard to navigate different resources to find accurate information. We're glad to be supporting ShelterTech and the broader community to build SF Service Guide - a one-stop, reliable place to help connect residents to the services they need"`,
-  author: "—Barry Roeder, Mayor's Office of Housing and Community Development",
+  quote:
+    "We've heard from San Franciscans, including as part of our recent strategic planning community outreach that it's hard to know where to go when you're looking for supportive services. It's hard to know where to start, and hard to navigate different resources to find accurate information. We're glad to be supporting ShelterTech and the broader community to build SF Service Guide - a one-stop, reliable place to help connect residents to the services they need.",
+  attribution:
+    "Barry Roeder, Mayor's Office of Housing and Community Development",
 };

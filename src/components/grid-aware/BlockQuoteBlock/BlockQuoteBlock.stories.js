943475e61b3a54ab48e6949172ffaa2070148e40
Ivan Lee <ilee.code@gmail.com>
2020-11-03 14:35:50 -0800

    Create new default argument and changed parameter to quotes

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
@@ -6,15 +6,20 @@ export default {
   component: BlockQuoteBlock,
 };
 
-const Template = ({ quote, attribution }) => (
-  <BlockQuoteBlock quote={quote} attribution={attribution} />
+const Template = ({ quotes }) => (
+    <BlockQuoteBlock quotes={ quotes }/>
 );
 
 export const DefaultBlockQuoteBlock = Template.bind({});
 
 DefaultBlockQuoteBlock.args = {
-  quote:
-    "We've heard from San Franciscans, including as part of our recent strategic planning community outreach that it's hard to know where to go when you're looking for supportive services. It's hard to know where to start, and hard to navigate different resources to find accurate information. We're glad to be supporting ShelterTech and the broader community to build SF Service Guide - a one-stop, reliable place to help connect residents to the services they need.",
-  attribution:
-    "Barry Roeder, Mayor's Office of Housing and Community Development",
+  quotes:[
+    {quote:
+        "We've heard from San Franciscans, including as part of our recent strategic planning community outreach that it's hard to know where to go when you're looking for supportive services. It's hard to know where to start, and hard to navigate different resources to find accurate information. We're glad to be supporting ShelterTech and the broader community to build SF Service Guide - a one-stop, reliable place to help connect residents to the services they need.",
+      attribution:
+        "Barry Roeder, Mayor's Office of Housing and Community Development"},
+    { quote: "ShelterTech, Welcomes You!", attribution: "Richard Xia" },
+    { quote: "Please Donate!", attribution: "David Agustin" },
+    { quote: "Happy Holidays", attribution: "Candy Wang" },
+  ]
 };

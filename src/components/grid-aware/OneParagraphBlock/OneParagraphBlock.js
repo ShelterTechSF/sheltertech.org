d906f62615caceddd5bf9883a44495cec5100071
Candy Wang <candywang013@gmail.com>
2020-11-06 01:02:53 -0800

    Refactor component with new job prop

--- src/components/grid-aware/OneParagraphBlock/OneParagraphBlock.js
+++ src/components/grid-aware/OneParagraphBlock/OneParagraphBlock.js
@@ -5,60 +5,29 @@ import Button from "../../inline/Button";
 
 import s from "./OneParagraphBlock.module.css";
 
-/* PropType shapes */
-
-const JobPropType = PropTypes.shape({
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-});
-
-/* Subcomponents */
-
-const JobBlock = ({ title, description }) => (
-  <div className={s.jobItem}>
-    <div className={s.jobTitle}>{title}</div>
-    <div className={s.jobDescription}>{description}</div>
-  </div>
-);
-
-JobBlock.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-};
-
-const CTABlock = ({ button }) => (
-  <div>
-    <div className={s.ctaButtonWrapper}>
-      {button.map(({ text, internalLink, externalLink, onClick }) => (
-        <div key={text}>
-          <Button {...{ text, internalLink, externalLink, onClick }} />
-        </div>
-      ))}
-    </div>
-  </div>
-);
-
-CTABlock.propTypes = {
-  button: PropTypes.arrayOf(Button.propTypes).isRequired,
-};
-
-/* Main component */
-
-const OneParagraphBlock = ({ title, job1, job2, job3, job4, ctaButton }) => {
+const OneParagraphBlock = ({ title, jobs, ctaButton }) => {
   const GridAreaLeft = () => (
     <div className={s.gridAreaLeft}>
-      <h1 className={s.title}>{title}</h1>
+      <div className={s.title}>{title}</div>
     </div>
   );
 
   const GridAreaRight = () => (
     <div className={s.gridAreaRight}>
       <div className={s.jobsWrapper}>
-        <JobBlock title={job1.title} description={job1.description} />
-        <JobBlock title={job2.title} description={job2.description} />
-        <JobBlock title={job3.title} description={job3.description} />
-        <JobBlock title={job4.title} description={job4.description} />
-        <CTABlock button={ctaButton} />
+        <div className={s.jobs}>{jobs}</div>
+        <div className={s.ctaButtonWrapper}>
+          {ctaButton.map((button) => (
+            <div key={button.text}>
+              <Button
+                text={button.text}
+                internalLink={button.internalLink}
+                externalLink={button.externalLink}
+                onClick={button.onClick}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
@@ -78,11 +47,8 @@ const OneParagraphBlock = ({ title, job1, job2, job3, job4, ctaButton }) => {
 
 OneParagraphBlock.propTypes = {
   title: PropTypes.string.isRequired,
-  job1: JobPropType.isRequired,
-  job2: JobPropType.isRequired,
-  job3: JobPropType.isRequired,
-  job4: JobPropType.isRequired,
-  ctaButton: PropTypes.arrayOf(Button.propTypes).isRequired,
+  jobs: PropTypes.node.isRequired,
+  ctaButton: PropTypes.shape(Button.propTypes).isRequired,
 };
 
 export default OneParagraphBlock;

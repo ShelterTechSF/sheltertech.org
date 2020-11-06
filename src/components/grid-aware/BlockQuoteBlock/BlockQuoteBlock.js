508aadde76747c420c82d4204dfb20d46d8fb6f7
Ivan Lee <ilee.code@gmail.com>
2020-11-06 11:12:40 -0800

    src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.js
@@ -1,44 +1,46 @@
 import PropTypes from "prop-types";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  DotGroup,
+  Dot,
+} from "pure-react-carousel";
 import React from "react";
 
-import { CarouselProvider, Slider, Slide, DotGroup } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import "pure-react-carousel/dist/react-carousel.es.css";
 import s from "./BlockQuoteBlock.module.css";
 
 const BlockQuoteBlock = ({ quotes }) => {
-
-  const Carousel =  () => (
-    <CarouselProvider
-    totalSlides={quotes.length}
-    isIntrinsicHeight={true}
-  >
-    <Slider>
-      { quotes.map((dataObj,index) => (
-        <Slide index={index} key={dataObj.quote}>
-          <figure className={s.innerWrapper}>
-            <blockquote 
-              className={s.quote} >&ldquo;{dataObj.quote}&rdquo;
-            </blockquote>
-            <figcaption className={s.attribution}>
-              &mdash;{dataObj.attribution}
-            </figcaption>
-          </figure>
-        </Slide>
-      )
-    )}
-    </Slider>
-    <DotGroup/>
-  </CarouselProvider>
-  )
+  const Carousel = () => (
+    <CarouselProvider totalSlides={quotes.length} isIntrinsicHeight>
+      <Slider>
+        {quotes.map((dataObj, index) => (
+          <Slide index={index} key={dataObj.quote}>
+            <figure className={s.innerWrapper}>
+              <blockquote className={s.quote}>
+                &ldquo;{dataObj.quote}&rdquo;
+              </blockquote>
+              <figcaption className={s.attribution}>
+                &mdash;{dataObj.attribution}
+              </figcaption>
+            </figure>
+          </Slide>
+        ))}
+      </Slider>
+      <DotGroup className={s.dots} />
+    </CarouselProvider>
+  );
 
   return (
     <div className={s.bleedWrapper}>
-    <div className={s.bleedMainContent}>
-      <div className={s.gridAreaMiddle}>
-        <Carousel/>
+      <div className={s.bleedBackground} />
+      <div className={s.gridParent}>
+        <div className={s.gridAreaMiddle}>
+          <Carousel />
+        </div>
       </div>
     </div>
-  </div>   
   );
 };
 

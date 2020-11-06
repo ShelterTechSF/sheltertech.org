466d31bbbbb0d7f5acc78bfbcdb1780bd866cba3
Candy Wang <candywang013@gmail.com>
2020-11-06 02:56:19 -0800

    Use Figma Designs - MVP text and image

--- src/pages/new/volunteer/index.js
+++ src/pages/new/volunteer/index.js
@@ -1,49 +1,39 @@
 import React from "react";
 
 import TwoParagraphBlock from "../../../components/grid-aware/TwoParagraphBlock";
-import darcelHeadshot from "../../../components/grid-aware/TwoParagraphBlock/stories/darcel-jackson-headshot.png";
+import volunteers from "../../../components/grid-aware/TwoParagraphBlock/stories/volunteers.png";
 import Layout from "../../../components/layout";
 
 export default () => (
   <Layout>
     <TwoParagraphBlock
-      title="Rooted in the Community"
-      paragraph1="ShelterTech was founded in 2016 by Darcel Jackson, who experienced homelessness first hand. Darcel envisioned a Bay Area where technology serves everyone, and those who were most vulnerable had the digital infrastructure and tools they need to get back on their feet."
+      title="What it’s like to volunteer at ShelterTech"
+      paragraph1="No matter what role you play, the essential attitude you need is a deep belief in digital equity for all people."
       paragraph2={
         <>
+          <p>We ask that you consider these three things before joining:</p>
           <p>
-            Our all volunteer team is paving the way for a new kind of community
-            organisation, one where expertise is shared across income boundaries
-            and life experiences. From day one, we’ve worked side-by-side with
-            those experiencing homelessness to understand the challenges they
-            face and the solutions they want to address their needs.
+            &ndash; You are willing to volunteer at least three hours each week.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            &ndash; You will be kind, considerate, and ethical towards other
+            volunteers and the communities we serve.
           </p>
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab inventore veritatis et quasi architecto beatae vitae dicta
-            sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt.
+            &ndash; You understand that ShelterTech is an all-volunteer team,
+            and that you will try your best to always represent ShelterTech in a
+            professional and thoughftul manner.
           </p>
         </>
       }
       image={{
-        url: darcelHeadshot,
-        alt: "Darcel Jackson head shot wearing a heather gray hoodie.",
+        url: volunteers,
+        alt:
+          "Five volunteer members surrounding a table of holiday care packages.",
       }}
-      ctaButton={[
+      ctaButtons={[
         {
-          text: "More About us",
+          text: "Apply",
           internalLink: "/foo",
         },
       ]}

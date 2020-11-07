ef8411414f72627d532a235afc9f22ae4fedb897
Ivan Lee <ilee.code@gmail.com>
2020-11-07 12:43:09 -0800

    Change term percent to number

--- src/components/grid-aware/StatsBlock/StatsBlock.js
+++ src/components/grid-aware/StatsBlock/StatsBlock.js
@@ -3,14 +3,14 @@ import React from "react";
 
 import s from "./StatsBlock.module.css";
 
-const StatsBlock = ({ title, percentAndStatement }) => {
+const StatsBlock = ({ title, numberAndStatement }) => {
   const Title = () => <h1 className={s.title}>{title}</h1>;
 
-  const PercentAndStatement = () => (
+  const NumberAndStatement = () => (
     <div className={s.gridAreaBottom}>
-      {percentAndStatement.map((obj) => (
+      {numberAndStatement.map((obj) => (
         <div>
-          <div className={s.percent}>{obj.percent}</div>
+          <div className={s.number}>{obj.number}</div>
           <div className={s.statement}>{obj.statement}</div>
         </div>
       ))}
@@ -23,7 +23,7 @@ const StatsBlock = ({ title, percentAndStatement }) => {
         <section className={s.gridParent}>
           <div className={s.allTextWrap}>
             <Title />
-            <PercentAndStatement />
+            <NumberAndStatement />
           </div>
         </section>
       </div>
@@ -31,13 +31,13 @@ const StatsBlock = ({ title, percentAndStatement }) => {
   );
 };
 
-const percentAndStatement = PropTypes.shape({
-  percent: PropTypes.string.isRequired,
+const numberAndStatement = PropTypes.shape({
+  number: PropTypes.string.isRequired,
   statement: PropTypes.string.isRequired,
 });
 StatsBlock.propTypes = {
   title: PropTypes.string.isRequired,
-  percentAndStatement: PropTypes.arrayOf(percentAndStatement).isRequired,
+  numberAndStatement: PropTypes.arrayOf(numberAndStatement).isRequired,
 };
 
 export default StatsBlock;
--- src/components/grid-aware/StatsBlock/StatsBlock.stories.js
+++ src/components/grid-aware/StatsBlock/StatsBlock.stories.js
@@ -6,27 +6,27 @@ export default {
   component: StatsBlock,
 };
 
-const Template = ({ title, percentAndStatement }) => (
-  <StatsBlock title={title} percentAndStatement={percentAndStatement} />
+const Template = ({ title, numberAndStatement }) => (
+  <StatsBlock title={title} numberAndStatement={numberAndStatement} />
 );
 
 export const DefaultStatsBlock = Template.bind({});
 
 DefaultStatsBlock.args = {
   title: "See what our volunteers have accomplished together.",
-  percentAndStatement: [
+  numberAndStatement: [
     {
-      percent: "9,000",
+      number: "9,000",
       statement:
         "At-risk community members can now access services and amenities",
     },
     {
-      percent: "560",
+      number: "560",
       statement:
         "Bay Area volunteers have contributed to our cause since our founding in 2016",
     },
     {
-      percent: "$2M",
+      number: "$2M",
       statement:
         "Worth of work provided by volunteers has been put towards our programs",
     },

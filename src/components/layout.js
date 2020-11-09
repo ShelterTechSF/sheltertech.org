4233177b27048600733ba0920cd0e727a8ec971d
David Agustin <dagustin@myvest.com>
2020-11-09 03:07:02 -0800

    Fix layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,13 @@ import React, { useState, useEffect } from "react";
 import ReactModal from "react-modal";
 
 import "../stylesheets/global.css";
+import shelterTechLogoWhite from "./grid-aware/Footer/sheltertech-logo-white.svg";
 import Footer from "./grid-aware/Footer";
+import facebookLogo from "./grid-aware/Footer/stories/facebook.svg";
+import githubLogo from "./grid-aware/Footer/stories/github.svg";
+import instagramLogo from "./grid-aware/Footer/stories/instagram.svg";
+import sealOfTransparency from "./grid-aware/Footer/stories/seal-of-transparency.svg";
+import twitterLogo from "./grid-aware/Footer/stories/twitter.svg";
 import { BurgerMenu, Navigation } from "./grid-aware/Navigation";
 
 const navigationItems = [
@@ -36,7 +42,64 @@ const Layout = ({ children }) => {
         />
         {children}
       </div>
-      <Footer />
+      <Footer
+        inputPlaceHolderText="email address"
+        footerNavigation={[
+          {
+            title: "Our Work",
+            links: [
+              { name: "ShelterConnect", url: "/shelterconnect" },
+              { name: "SF Services Guide", url: "/sfservicesguide" },
+              { name: "Community Reps", url: "/communityreps" },
+            ],
+          },
+          {
+            title: "What You Can Do",
+            links: [
+              { name: "Volunteer", url: "/new/volunteer" },
+              { name: "Donate", url: "/new/donate" },
+              { name: "Partnerships", url: "/partnerships" },
+            ],
+          },
+          {
+            title: "About Us",
+            links: [
+              { name: "Our Story", url: "/ourstory" },
+              { name: "Record", url: "/record" },
+              { name: "Newsroom", url: "/newsroom" },
+            ],
+          },
+        ]}
+        seals={[{ logo: sealOfTransparency, alt: "Seal of Transparency" }]}
+        shelterTechLogo={{
+          logo: shelterTechLogoWhite,
+          alt: "ShelterTech Logo",
+        }}
+        socialMediaLinks={[
+          {
+            url: "https://www.facebook.com/ShelterTechOrg/",
+            logo: facebookLogo,
+            alt: "Facebook Logo",
+          },
+          {
+            url: "https://twitter.com/sheltertechorg",
+            logo: twitterLogo,
+            alt: "Twitter Logo",
+          },
+          {
+            url: "https://www.instagram.com/shelter_tech",
+            logo: instagramLogo,
+            alt: "Instagram Logo",
+          },
+          {
+            url: "https://github.com/ShelterTechSF",
+            logo: githubLogo,
+            alt: "GitHub Logo",
+          },
+        ]}
+        address="268 Bush Street #4302, San Francisco CA, 94104 USA"
+        ein="• ein: 38-3984099"
+      />
     </div>
   );
 };

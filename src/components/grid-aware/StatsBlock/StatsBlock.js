93a86766e8248958be91ce6c67b0cf3db04af5db
Ivan Lee <ilee.code@gmail.com>
2020-11-09 22:50:33 -0800

    Add StatCard component

--- src/components/grid-aware/StatsBlock/StatsBlock.js
+++ src/components/grid-aware/StatsBlock/StatsBlock.js
@@ -2,34 +2,31 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import s from "./StatsBlock.module.css";
+const StatCard = () => (
+  <div>
+    <div className={s.number}>{obj.number}</div>
+    <div className={s.statement}>{obj.statement}</div>
+  </div>
+)
 
-const StatsBlock = ({ title, numberAndStatement }) => {
-  const Title = () => <h1 className={s.title}>{title}</h1>;
-
-  const NumberAndStatement = () => (
-    <div className={s.gridAreaBottom}>
-      {numberAndStatement.map((obj) => (
-        <div key={obj.number}>
-          <div className={s.number}>{obj.number}</div>
-          <div className={s.statement}>{obj.statement}</div>
-        </div>
-      ))}
-    </div>
-  );
-
-  return (
+const StatsBlock = ({ title, numberAndStatement }) => (
     <div className={s.bleedWrapper}>
       <div className={s.bleedMainContent}>
         <section className={s.gridParent}>
           <div className={s.allTextWrap}>
-            <Title />
-            <NumberAndStatement />
+            <h1 className={s.title}>{title}</h1>
+            <div className={s.gridAreaBottom}>
+              {numberAndStatement.map((obj) => (
+                <div key={obj.number}>
+                  <StatCard />
+                </div>
+              ))}
+            </div>
           </div>
         </section>
       </div>
     </div>
   );
-};
 
 const numberAndStatement = PropTypes.shape({
   number: PropTypes.string.isRequired,

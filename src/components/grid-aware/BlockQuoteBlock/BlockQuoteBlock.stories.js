9cb08de49163eaf26cd7cb60c9163120e389b930
Ivan Lee <ilee.code@gmail.com>
2020-11-09 22:57:56 -0800

    Add lint changes

--- src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
+++ src/components/grid-aware/BlockQuoteBlock/BlockQuoteBlock.stories.js
@@ -20,12 +20,6 @@ DefaultBlockQuoteBlock.args = {
     },
     {
       quote:
-        "We’ve heard from San Franciscans, including as part of our recent strategic planning community outreach that It’s hard to know where to go when you’re looking for supportive services. It’s hard to know where to start, and hard to navigate different resources to find accurate information. We’re glad to be supporting ShelterTech and the broader community to build SF Service Guide – a one-stop, reliable place to help connect residents to the services they need.",
-      attribution:
-        "Barry Roeder, Mayor’s Office of Housing and Community Development",
-    },
-    {
-      quote:
         "Wi-fi has been a top request among young people here at Larkin Street. Now that we have it with ShelterTech we are seeing more young people come in and stay around and get the things they need so that they can move past homelessness, permanently.",
       attribution:
         "Veronica Pastore, Director of Communications, Larkin Street Youth Services",

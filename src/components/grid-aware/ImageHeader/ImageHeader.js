7284b4b82e99cfa63cfbab9107f11428d6b1d04c
Candy Wang <candywang013@gmail.com>
2020-11-09 23:59:28 -0800

    Rename class selectors, remove unused properties in css

--- src/components/grid-aware/ImageHeader/ImageHeader.js
+++ src/components/grid-aware/ImageHeader/ImageHeader.js
@@ -16,15 +16,15 @@ const ImageHeader = ({
   description,
   image1,
   image2,
-  ctaButton,
+  ctaButtons,
 }) => {
   const GridAreaLeft = () => (
-    <div className={s.gridAreaLeft}>
-      <h1 className={s.titleWrapper}>{title}</h1>
-      <div className={s.subtitleWrapper}>{subtitle}</div>
-      <div className={s.descriptionWrapper}>{description}</div>
-      <div className={s.ctaButtonWrapper}>
-        {ctaButton.map((button) => (
+    <div className={s.gridAreaText}>
+      <h1 className={s.title}>{title}</h1>
+      <div className={s.subtitle}>{subtitle}</div>
+      <div className={s.description}>{description}</div>
+      <div className={s.ctaButtonRow}>
+        {ctaButtons.map((button) => (
           <div key={button.text}>
             <Button
               text={button.text}
@@ -42,13 +42,13 @@ const ImageHeader = ({
     <div className={s.bleedWrapper}>
       <div className={s.bleedMainContent}>
         <section className={s.gridParent}>
-          <div className={s.gridAreaRight}>
+          <div className={s.gridAreaImage1}>
             <div className={s.image1Wrapper}>
               <img className={s.image} src={image1.url} alt={image1.alt} />
             </div>
           </div>
           <GridAreaLeft />
-          <div className={s.gridAreaRight}>
+          <div className={s.gridAreaImage2}>
             <div className={s.image2Wrapper}>
               <img className={s.image} src={image2.url} alt={image2.alt} />
             </div>
@@ -65,7 +65,7 @@ ImageHeader.propTypes = {
   description: PropTypes.string.isRequired,
   image1: ImagePropType.isRequired,
   image2: ImagePropType.isRequired,
-  ctaButton: PropTypes.arrayOf(Button.propTypes).isRequired,
+  ctaButtons: PropTypes.arrayOf(Button.propTypes).isRequired,
 };
 
 export default ImageHeader;

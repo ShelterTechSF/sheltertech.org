a5b8805ba5fe91201295cf2c65f2df4369e34717
Ivan Lee <ilee.code@gmail.com>
2020-11-10 20:26:11 -0800

    Add statcard and statcards components with own proptypes

--- src/components/grid-aware/StatsBlock/StatsBlock.js
+++ src/components/grid-aware/StatsBlock/StatsBlock.js
@@ -2,39 +2,44 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import s from "./StatsBlock.module.css";
-const StatCard = ({ number, statement }) => (
-  <div>
-    <div className={s.number}>{number}</div>
-    <div className={s.statement}>{statement}</div>
-  </div>
-)
 
-const StatsBlock = ({ title, numberAndStatement }) => (
-    <div className={s.bleedWrapper}>
-      <div className={s.bleedMainContent}>
-        <section className={s.gridParent}>
-          <div className={s.allTextWrap}>
-            <h1 className={s.title}>{title}</h1>
-            <div className={s.gridAreaBottom}>
-              {numberAndStatement.map((obj) => (
-                <div key={obj.number}>
-                  <StatCard number={obj.number} statement={obj.statement}/>
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
-      </div>
-    </div>
-  );
-
-const numberAndStatement = PropTypes.shape({
+const StatCards = PropTypes.shape({
   number: PropTypes.string.isRequired,
   statement: PropTypes.string.isRequired,
 });
+
+const StatCard = ({ number, statement }) => (
+  <div className={s.statCard}>
+    <div className={s.number}>{number}</div>
+    <div className={s.statement}>{statement}</div>
+  </div>
+);
+
+StatCard.propTypes = {
+  number: PropTypes.string.isRequired,
+  statement: PropTypes.string.isRequired,
+};
+
+const StatsBlock = ({ title, statCards }) => (
+  <div className={s.bleedWrapper}>
+    <div className={s.bleedMainContent}>
+      <section className={s.gridParent}>
+        <h1 className={s.title}>{title}</h1>
+        <div className={s.gridAreaBottom}>
+          {statCards.map((obj) => (
+            <div key={obj.number}>
+              <StatCard number={obj.number} statement={obj.statement} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  </div>
+);
+
 StatsBlock.propTypes = {
   title: PropTypes.string.isRequired,
-  numberAndStatement: PropTypes.arrayOf(numberAndStatement).isRequired,
+  statCards: PropTypes.arrayOf(StatCards).isRequired,
 };
 
 export default StatsBlock;

4cb235c63b9f7a6dbe11a71ecfeb854b0a8faf92
Candy Wang <candywang013@gmail.com>
2020-11-11 02:46:16 -0800

    Instantiate OneParagraphBlock component onto Volunteer page

--- src/pages/new/volunteer/index.js
+++ src/pages/new/volunteer/index.js
@@ -6,34 +6,36 @@ import Layout from "../../../components/layout";
 export default () => (
   <Layout>
     <OneParagraphBlock
-      title="Right now, we are looking for:"
+      title="This is where we could use your expertise"
       jobs={
         <>
-          <div>
-            Product managers, designers, data scientists, and user researchers
-          </div>
-          <div>
-            Work with research, product, and dev teams to create digital
+          <p>Product</p>
+          <p>
+            Our department—which includes product managers, designers, data
+            scientists, and user researchers—creates and maintains digital
             services with real impact.
-          </div>
-          <div>Front & Back-end Developers</div>
-          <div>
-            Full stack, Front End, and Dev Ops opportunities (React or Ruby on
-            Rails preferred).
-          </div>
-          <div>Growth & Marketing</div>
-          <div>
-            Build and execute our GTM strategy for SF Service Guide amongst
-            service providers and low and no-income San Franciscans.
-          </div>
-          <div>Fundraising & Partnerships</div>
-          <div>
-            Help us fundraise for our ShelterConnect installations and
-            empowering all our teams to do more
-          </div>
+          </p>
+          <p>Marketing and Growth</p>
+          <p>
+            This dynamic team builds awareness by following a communications
+            roadmap and executing our go-to-market strategy for service
+            providers and unsung San Franciscans.
+          </p>
+          <p>Fundraising and Partnerships</p>
+          <p>
+            Help us organize events and campaigns to raise donations for our
+            ShelterConnect installations that provide free wifi and hardware for
+            local housing shelters.
+          </p>
+          <p>Fundraising & Partnerships</p>
+          <p>
+            To create user-friendly websites, we need front- and back-end
+            developers and dev ops professionals who know React or Ruby on
+            Rails.
+          </p>
         </>
       }
-      ctaButton={[{ text: "Become a Volunteer", internalLink: "/foo" }]}
+      ctaButton={[{ text: "Apply", internalLink: "/foo" }]}
     />
   </Layout>
 );

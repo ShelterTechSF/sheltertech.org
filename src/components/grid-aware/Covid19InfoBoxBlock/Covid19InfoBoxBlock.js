e392ef397aebbbb7fe0408f80eebf8fb2c87d607
Ivan Lee <ilee.code@gmail.com>
2020-11-11 10:25:57 -0800

    Adding new Covid19 component

---
+++ src/components/grid-aware/Covid19InfoBoxBlock/Covid19InfoBoxBlock.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import s from "./Covid19InfoBoxBlock.module.css";
+
+const Covid19InfoBoxBlock = ({ title, description }) => {
+  return (
+    <div className={s.bleedWrapper}>
+      <div className={s.bleedMainContent}>
+        <div className={s.textWrapper}>
+          <div className={s.title}>{title}</div>
+          <div className={s.description}>{description}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Covid19InfoBoxBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Covid19InfoBoxBlock;

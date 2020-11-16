d9c6358e2ef543b3cc24f7b89d09fc5aa8c2a833
Candy Wang <candywang013@gmail.com>
2020-11-16 05:23:49 -0800

    Fix import statements and implement TitleDescriptionPropType

--- src/legacy/layouts/Layout.js
+++ src/legacy/layouts/Layout.js
@@ -1,4 +1,5 @@
 import { StaticQuery, graphql } from "gatsby";
+import PropTypes from "prop-types";
 import React from "react";
 import { Helmet } from "react-helmet";
 import { TitleDescriptionPropType } from "../../PropTypeShapes";

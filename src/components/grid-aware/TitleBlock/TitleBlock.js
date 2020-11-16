2df1f532750601956b0fb63344f53a65c05ab31f
Ivan Lee <ilee.code@gmail.com>
2020-11-15 20:22:18 -0800

    Implement Title Block

---
+++ src/components/grid-aware/TitleBlock/TitleBlock.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import s from "./TitleBlock.module.css";
+
+const TitleBlock = ({ title }) => {
+  return (
+    <div className={s.bleedWrapper}>
+      <div className={s.bleedMainContent}>
+        <div className={s.gridParent}>
+          <div className={s.textWrapper}>
+            <div className={s.title}>{title}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+TitleBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default TitleBlock;

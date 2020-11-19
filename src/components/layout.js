cd73e59c34b8036965c35c5ccb447627fea726db
David Agustin <dagustin@myvest.com>
2020-11-18 23:25:45 -0800

    Move footer into page-wrapper div

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,52 +41,52 @@ const Layout = ({ children }) => {
           toggleBurgerMenu={() => setBurgerMenuIsOpen(!burgerMenuIsOpen)}
         />
         {children}
+        <Footer
+          formInputPlaceholder="email address"
+          formInputValue="email"
+          formDescription="Subscribe to get updates"
+          footerNavigation={[
+            { text: "Programs", internalLink: "/new" },
+            { text: "Volunteer", internalLink: "/new/volunteer" },
+            { text: "Donate", internalLink: "/new/donate" },
+          ]}
+          seals={[
+            {
+              link: "https://www.guidestar.org/profile/38-3984099",
+              logo: sealOfTransparency,
+              alt: "GuideStar Seal of Transparency",
+            },
+          ]}
+          shelterTechLogo={{
+            url: shelterTechLogoWhite,
+            alt: "ShelterTech Logo",
+          }}
+          socialMediaLinks={[
+            {
+              link: "https://www.facebook.com/ShelterTechOrg/",
+              logo: facebookLogo,
+              alt: "Facebook Logo",
+            },
+            {
+              link: "https://twitter.com/sheltertechorg",
+              logo: twitterLogo,
+              alt: "Twitter Logo",
+            },
+            {
+              link: "https://www.instagram.com/shelter_tech",
+              logo: instagramLogo,
+              alt: "Instagram Logo",
+            },
+            {
+              link: "https://github.com/ShelterTechSF",
+              logo: githubLogo,
+              alt: "GitHub Logo",
+            },
+          ]}
+          address="268 Bush Street #4302, San Francisco CA, 94104 USA"
+          employerIdentificationNumber="ein: 38-3984099"
+        />
       </div>
-      <Footer
-        formInputPlaceholder="email address"
-        formInputValue="email"
-        formDescription="Subscribe to get updates"
-        footerNavigation={[
-          { text: "Programs", internalLink: "/new" },
-          { text: "Volunteer", internalLink: "/new/volunteer" },
-          { text: "Donate", internalLink: "/new/donate" },
-        ]}
-        seals={[
-          {
-            link: "https://www.guidestar.org/profile/38-3984099",
-            logo: sealOfTransparency,
-            alt: "GuideStar Seal of Transparency",
-          },
-        ]}
-        shelterTechLogo={{
-          url: shelterTechLogoWhite,
-          alt: "ShelterTech Logo",
-        }}
-        socialMediaLinks={[
-          {
-            link: "https://www.facebook.com/ShelterTechOrg/",
-            logo: facebookLogo,
-            alt: "Facebook Logo",
-          },
-          {
-            link: "https://twitter.com/sheltertechorg",
-            logo: twitterLogo,
-            alt: "Twitter Logo",
-          },
-          {
-            link: "https://www.instagram.com/shelter_tech",
-            logo: instagramLogo,
-            alt: "Instagram Logo",
-          },
-          {
-            link: "https://github.com/ShelterTechSF",
-            logo: githubLogo,
-            alt: "GitHub Logo",
-          },
-        ]}
-        address="268 Bush Street #4302, San Francisco CA, 94104 USA"
-        employerIdentificationNumber="ein: 38-3984099"
-      />
     </div>
   );
 };

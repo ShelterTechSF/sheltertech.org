d2b60401b4288bfe8e80ec9d0293a3e593e9164b
David Agustin <dagustin@myvest.com>
2020-11-23 20:46:58 -0800

    Remove import

--- src/pages/donate/index.js
+++ src/pages/donate/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
 
 import ArticleSpotlightCard from "../../components/grid-aware/ArticleSpotlightCard";

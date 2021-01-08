74b6fb6d19fae8b5fdf7dd2de8ac176f1b7ae2a1
Jessica Peck <jessypeck@gmail.com>
2021-01-07 18:10:01 -0800

    Add bounds to map & hide zoom/fullscreen buttons

--- src/components/block/InteractiveMap/InteractiveMap.js
+++ src/components/block/InteractiveMap/InteractiveMap.js
@@ -2,12 +2,31 @@ import GoogleMapReact from "google-map-react";
 import PropTypes from "prop-types";
 import React from "react";
 
+// Callback used by google-map-react library to allow access to additional configuration options.
+// https://github.com/google-map-react/google-map-react/blob/master/API.md#options-funcobject
+const createMapOptions = () => {
+  return {
+    zoomControl: false,
+    fullscreenControl: false,
+    restriction: {
+      latLngBounds: {
+        north: 37.82,
+        south: 37.70,
+        west: -122.53,
+        east: -122.37,
+      },
+      strictBounds: false,
+    },
+  };
+};
+
 const InteractiveMap = ({ center, zoom }) => {
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.GOOGLE_API_KEY }}
       defaultCenter={center}
       defaultZoom={zoom}
+      options={createMapOptions}
     />
   );
 };

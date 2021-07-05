ddf77688cabf3c6d8b49c55549a58d3994cff7d4
Candy Wang <candywang013@gmail.com>
2021-07-04 19:54:46 -0700

    Add 'About Us' navigation item and change to root path

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -20,7 +20,8 @@ type NavItem = {
 };
 
 const navigationItems: NavItem[] = [
-  { text: "Programs", internalLink: "/" },
+  { text: "About Us", internalLink: "/" },
+  { text: "Programs", internalLink: "/programs" },
   { text: "Volunteer", internalLink: "/volunteer" },
   { text: "Donate", internalLink: "/donate", isButton: true },
 ];
@@ -85,7 +86,8 @@ const Layout = ({ children }: LayoutProps) => {
           formAntiBotInputName="b_c47829732a0bea5c8e8a94604_08f60e42ef"
           formDescription="Subscribe to get updates"
           footerNavigation={[
-            { text: "Programs", internalLink: "/" },
+            { text: "About Us", internalLink: "/" },
+            { text: "Programs", internalLink: "/programs" },
             { text: "Volunteer", internalLink: "/volunteer" },
             { text: "Donate", internalLink: "/donate" },
           ]}

b5bd7e029a5e403d4cbcf64d65c8fdebd999d9cd
Candy Wang <candywang013@gmail.com>
2021-07-05 01:43:21 -0700

    Add TwoParagraphBlock component

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,14 +3,39 @@ import { Helmet } from "react-helmet";
 
 import ArticleSpotlightCard from "../../components/grid-aware/ArticleSpotlightCard";
 import Spacer from "../../components/grid-aware/Spacer";
+import TwoParagraphBlock from "../../components/grid-aware/TwoParagraphBlock";
 import Layout from "../../components/layout";
 import articleSpotlightImage from "../volunteer/laura-barerra-vera.png";
+import darcelJackson from "./darcel-jackson.png";
 
 export default () => (
   <Layout>
     <Helmet>
       <title>About Us | ShelterTech</title>
     </Helmet>
+    <TwoParagraphBlock
+      title="A note from our Founder, Darcel Jackson"
+      paragraph2={
+        <>
+          <p>
+            I became homeless when I was injured and lost my welding job working
+            on the new Bay Bridge in San Francisco. I realized the longer I
+            stayed in that condition, the harder it would become to escape it.
+            Living on the streets, you spend most of your time just trying to
+            survive. It’s hard to know where to start, what services you need,
+            how to get training in a new field or search for jobs. I had a
+            smartphone and could find WiFi, but there weren't any apps or
+            websites to help me get off the streets. Now I want to give back and
+            help people that become homeless. Darcel founded ShelterTech to help
+            bridge this digital divide.
+          </p>
+        </>
+      }
+      image={{
+        url: darcelJackson,
+        alt: "Darcel Jackson, founder of ShelterTech.",
+      }}
+    />
     <Spacer heightDesktop="190px" heightMobile="80px" />
     <ArticleSpotlightCard
       eyebrowText="Volunteer Spotlight"

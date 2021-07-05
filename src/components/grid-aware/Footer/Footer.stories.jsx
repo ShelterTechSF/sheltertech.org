0b47dbe618200d3133ff81cf7d9252cda2a7d3a7
Candy Wang <candywang013@gmail.com>
2021-07-04 20:27:19 -0700

    Add 'About Us' navigation item and change to root path

--- src/components/grid-aware/Footer/Footer.stories.jsx
+++ src/components/grid-aware/Footer/Footer.stories.jsx
@@ -49,7 +49,8 @@ DefaultFooter.args = {
   formAntiBotInputName: "b_c47829732a0bea5c8e8a94604_08f60e42ef",
   formDescription: "Subscribe to get updates",
   footerNavigation: [
-    { text: "Programs", internalLink: "/new" },
+    { text: "About", internalLink: "/new" },
+    { text: "Programs", internalLink: "/new/programs" },
     { text: "Volunteer", internalLink: "/new/volunteer" },
     { text: "Donate", internalLink: "/new/donate" },
   ],

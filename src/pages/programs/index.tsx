c49d737ea1ead8c24b3a7e54366884698c355161
Ivan Lee <ilee.code@gmail.com>
2021-08-02 03:31:02 -0700

    Change ' to  to fix lint error

--- src/pages/programs/index.tsx
+++ src/pages/programs/index.tsx
@@ -39,14 +39,14 @@ export default () => {
             <p>
               The SF Service Guide is an online directory of human services in
               San Francisco that launched in November 2019 and currently serves
-              1,500 unique monthly users. The directory's focus is on
+              1,500 unique monthly users. The directory&apos;s focus is on
               homelessness and housing services but also covers a variety of
               other services, from education and legal aid to senior services
               and re-entry programs. Our goal is to help anyone with access to
               smartphone, tablet, or computer find services they need.
             </p>
             <p>
-              SF Service Guide is supported by a grant from the SF Mayor's
+              SF Service Guide is supported by a grant from the SF Mayor&apos;s
               Office of Housing and Community Development and verified for
               accuracy by our data partner, the Homeless Advocacy Project of the
               JDC.

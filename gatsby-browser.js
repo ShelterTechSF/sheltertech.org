a6b8f09312d5a07bbe7fae023dffb95e54eb8d4c
Jessica Peck <jessypeck@gmail.com>
2021-09-04 10:48:15 -0700

    Disable linting on prismic preview boilerplate Our linter rules break the behavior

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,11 @@
 // Boilerplate for enabling Prismic previews
 // See: https://prismic.io/docs/technologies/previews-gatsby
+/* eslint-disable */
 
 import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
 import * as React from "react";
 import "gatsby-plugin-prismic-previews/dist/styles.css";
 
-const wrapRootElement = ({ element }) => (
+export const wrapRootElement = ({ element }) => (
   <PrismicPreviewProvider>{element}</PrismicPreviewProvider>
-);
-
-export default wrapRootElement;
+)

d651f97fcd7b9ee5eb063ce0c5ee0ccb03f0bdc0
Candy Wang <candywang013@gmail.com>
2022-03-09 21:57:19 -0800

    Revert StatsBlock files

--- src/components/grid-aware/StatsBlock/StatsBlock.stories.jsx
+++ src/components/grid-aware/StatsBlock/StatsBlock.stories.jsx
@@ -7,11 +7,11 @@ export default {
   component: StatsBlock,
 };
 
-const Template = ({ title, subtitle, barStatCards, theme, titleColor }) => (
+const Template = ({ title, subtitle, statCards, theme, titleColor }) => (
   <StatsBlock
     title={title}
     subtitle={subtitle}
-    barStatCards={barStatCards}
+    statCards={statCards}
     theme={theme}
     titleColor={titleColor}
   />
@@ -20,57 +20,28 @@ const Template = ({ title, subtitle, barStatCards, theme, titleColor }) => (
 export const DefaultStatsBlock = Template.bind({});
 
 DefaultStatsBlock.args = {
-  title: "ShelterConnect",
-  subtitle: (
-    <div>
-      Delivering free, turn-key WiFi solutions to shelters and transitional
-      housing facilities with key partners.{" "}
-      <a
-        href="https://www.sheltertech.org/programs"
-        style={{ color: "var(--color-sheltertech-blue)" }}
-      >
-        Learn more about this program.
-      </a>
-    </div>
-  ),
-  barStatCards: [
+  title: "See what our volunteers have accomplished together.",
+  subtitle: "Optional subtitle.",
+  statCards: [
     {
-      axes: [
-        { x: 2019, y: 14 },
-        { x: 2020, y: 17 },
-        { x: 2021, y: 21 },
-      ],
-      legend: {
-        title: "Shelters Wired",
-        subtitle: "+4 YOY",
-      },
-      position: "vertical",
+      number: "9,000",
+      secondaryNumber: "+5 YOY",
+      statement:
+        "At-risk community members can now access services and amenities",
     },
     {
-      axes: [
-        { x: "PEAK", y: 2398 },
-        { x: "TARGET", y: 2428 },
-        { x: 2020, y: 1412 },
-      ],
-      legend: {
-        title: "People serviced daily",
-        subtitle: "Daytime",
-      },
-      position: "horizontal",
+      number: "560",
+      secondaryNumber: "+5 YOY",
+      statement:
+        "Bay Area volunteers have contributed to our cause since our founding in 2016",
     },
     {
-      axes: [
-        { x: "PEAK", y: 1761 },
-        { x: "TARGET", y: 1132 },
-        { x: 2020, y: 900 },
-      ],
-      legend: {
-        title: "People serviced daily",
-        subtitle: "Nighttime",
-      },
-      position: "horizontal",
+      number: "$2M",
+      secondaryNumber: "+5 YOY",
+      statement:
+        "Worth of work provided by volunteers has been put towards our programs",
     },
   ],
-  theme: "gray",
+  theme: "white",
   titleColor: "blue",
 };
--- src/components/grid-aware/StatsBlock/StatsBlock.tsx
+++ src/components/grid-aware/StatsBlock/StatsBlock.tsx
@@ -10,7 +10,7 @@ type StatCardProps = {
 };
 
 const StatCard = ({ number, secondaryNumber, statement }: StatCardProps) => (
-  <div className={s.card}>
+  <div className={s.statCard}>
     <div className={s.number}>{number}</div>
     {secondaryNumber && (
       <div className={s.secondaryNumber}>{secondaryNumber}</div>
@@ -19,61 +19,10 @@ const StatCard = ({ number, secondaryNumber, statement }: StatCardProps) => (
   </div>
 );
 
-type AxisProp = {
-  x: string | number;
-  y: number;
-};
-
-type BarStatProps = {
-  axes: AxisProp[];
-  legend: { title: string; subtitle: string };
-  position: string;
-};
-
-const BarStatCard = ({ axes, legend, position }: BarStatProps) => {
-  const isVertical = position === "vertical";
-  const statNumbers = axes.map((axis) => axis.y);
-  const maxNumber = Math.max(...statNumbers);
-  const fillPixels = statNumbers.map(
-    (number) => `${Math.round((number / maxNumber) * 100)}px`
-  );
-
-  return (
-    <div className={s.card}>
-      <div className={`${s.barStat} ${s[position]}`}>
-        {axes.map(({ x, y }, i) => {
-          const isYMaxNumber = y === maxNumber;
-          const yAxis = y.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-          return (
-            <div key={`${x}-${y}`} className={`${s.barItem} ${s[position]}`}>
-              <div className={`${s.xAxis} ${s[position]}`}>{x}</div>
-              <div
-                style={{
-                  width: isVertical ? "30px" : fillPixels[i],
-                  height: isVertical ? fillPixels[i] : "30px",
-                  backgroundColor: isYMaxNumber
-                    ? "var(--color-sheltertech-blue)"
-                    : "var(--color-gray-600)",
-                  opacity: isYMaxNumber ? "" : "0.5",
-                }}
-              />
-              <div className={`${s.yAxis} ${s[position]}`}>{yAxis}</div>
-            </div>
-          );
-        })}
-      </div>
-      <div className={`${s.statement} ${s[position]}`}>{legend.title}</div>
-      <div className={s.secondaryNumber}>{legend.subtitle}</div>
-    </div>
-  );
-};
-
 type StatsBlockProps = {
   title: string;
-  subtitle?: string | React.ReactNode;
-  barStatCards?: BarStatProps[];
-  statCards?: StatCardProps[];
+  subtitle?: string;
+  statCards: StatCardProps[];
   theme: ThemeColorOption;
   titleColor?: TitleColorOption;
 };
@@ -81,7 +30,6 @@ type StatsBlockProps = {
 const StatsBlock = ({
   title,
   subtitle,
-  barStatCards,
   statCards,
   theme,
   titleColor = "black",
@@ -99,24 +47,14 @@ const StatsBlock = ({
         <h1 className={`${s.title} ${s[titleColor]}`}>{title}</h1>
         {subtitle && <h2 className={s.subtitle}>{subtitle}</h2>}
         <div className={s.gridAreaBottom}>
-          {statCards &&
-            statCards.map(({ number, secondaryNumber, statement }) => (
-              <StatCard
-                key={statement}
-                number={number}
-                secondaryNumber={secondaryNumber}
-                statement={statement}
-              />
-            ))}
-          {barStatCards &&
-            barStatCards.map(({ axes, legend, position }) => (
-              <BarStatCard
-                key={`${legend.title}-${legend.subtitle}`}
-                axes={axes}
-                legend={legend}
-                position={position}
-              />
-            ))}
+          {statCards.map(({ number, secondaryNumber, statement }) => (
+            <StatCard
+              key={statement}
+              number={number}
+              secondaryNumber={secondaryNumber}
+              statement={statement}
+            />
+          ))}
         </div>
       </section>
     </div>
